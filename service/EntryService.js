3a0e31ca0e8be8b9ee921a70216894c8be3808dd
jonaslagoni <jonas-lt@live.dk>
2020-03-31 16:26:08 +0200

    Fixed

--- service/EntryService.js
+++ service/EntryService.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 /**
  * Adds a new entry
  *
@@ -8,46 +7,65 @@
  * no response value expected for this operation
  **/
 exports.addEntry = function(body) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
-
+	return new Promise(function(resolve, reject) {
+		fs.readFile(path.resolve(pathToFile), 'utf8', function readFileCallback(
+			err,
+			data
+		) {
+			console.log(body);
+			if (err) {
+				console.log(err);
+				resolve();
+			} else {
+				let obj = JSON.parse(data); //now it an object
+				body['receivedDate'] = Math.round(Date.now() / 1000);
+				obj.push(body); //add some data
+				let jsonString = JSON.stringify(obj); //convert it back to json
+				fs.writeFile(
+					path.resolve(pathToFile),
+					jsonString,
+					'utf8',
+					(err, data) => {
+						if (err) {
+							console.log(err);
+						}
+						resolve();
+					}
+				);
+			}
+		});
+	});
+};
 
 /**
  *
  * returns List
  **/
 exports.entryGetEntriesGET = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "timeStamp" : "timeStamp",
-  "temp" : 6,
-  "lumen" : 0,
-  "receivedDate" : "receivedDate"
-}, {
-  "timeStamp" : "timeStamp",
-  "temp" : 6,
-  "lumen" : 0,
-  "receivedDate" : "receivedDate"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
-
+	return new Promise(function(resolve, reject) {
+		fs.readFile(path.resolve(pathToFile), 'utf8', function(err, data) {
+			if (err) {
+				console.log(err);
+				resolve();
+			} else {
+				console.log(data);
+				resolve(JSON.parse(data));
+			}
+		});
+	});
+};
 
 /**
  *
  * no response value expected for this operation
  **/
 exports.resetEntriesPOST = function() {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
-
+	return new Promise(function(resolve, reject) {
+		fs.writeFile(path.resolve(pathToFile), '[]', 'utf8', (err, data) => {
+			if (err) {
+				console.log(err);
+			}
+			resolve();
+		});
+	});
+};
